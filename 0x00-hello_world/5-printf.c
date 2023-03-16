16a9b8c702babc676ab46265c563498b9f3f939b
OukoFranchez <oukofranchez@gmail.com>
2023-03-16 22:55:03 +0300

    printing out a string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0(Success)
+*/
+int main(void)
+{
+/*printing a string using printf*/
+	printf("with proper grammar, but the outcome is a piec of art,");
+
+	return (0);
+}
