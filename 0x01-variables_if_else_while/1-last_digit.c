33333df7e61f535747be3c22b4ad4b8f909f7d41
OukoFranchez <oukofranchez@gmail.com>
2023-03-17 17:18:04 +0300

    corrections to match betty style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int n;
-	
+
 	int last_digit;
 
 	srand(time(0));
@@ -22,10 +22,12 @@ int main(void)
 	if (last_digit > 5)
 	{	printf("%d and is greater than 5", last_digit);
 	}
-	else if (last_digit == 0){
+	else if (last_digit == 0)
+	{
 		printf("%d and is 0", last_digit);
 	}
-	else if (last_digit < 6 && last_digit != 0){
+	else if (last_digit < 6 && last_digit != 0)
+	{
 		printf("%d and is less than 6 and not 0", last_digit);
 	}
 	return (0);
