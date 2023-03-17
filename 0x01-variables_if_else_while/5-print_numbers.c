54c07c03448387414fd2444bf0f2ff213f98e697
OukoFranchez <oukofranchez@gmail.com>
2023-03-17 19:09:07 +0300

    changing the program to start count from 0 instead

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,13 @@
 /**
  * main - Entry Point
  *
- * This program prints the numbers from 1 to 9, followed by a new line.
+ * This program prints the numbers from 0 to 9, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int number = 1;
+	int number = 0;
 
 	while (number < 10)
 	{
