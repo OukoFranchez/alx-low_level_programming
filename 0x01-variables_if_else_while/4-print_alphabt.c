9ae408502883b045c365047f831d173bb1d152ea
OukoFranchez <oukofranchez@gmail.com>
2023-03-17 18:35:39 +0300

    programm printing all lowercase alphabet letters in exception of two q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		if (lowercase != 'q' && lowercase != 'e')
+		{
+			putchar(lowercase);
+		}
+		lowercase++;
+		
+		}
+	putchar('\n');
+	
+	return (0);
+}
