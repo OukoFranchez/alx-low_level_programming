0fcb4f0546fbe44139a23d179960e5934bf67e7f
OukoFranchez <oukofranchez@gmail.com>
2023-03-18 05:00:53 +0300

    correction on the code

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,18 +24,15 @@ int main(void)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
-					if (i == 7 && j == 8 && k == 9)
-					{
-						putchar(k + '0');
-						putchar('\n');
-					}
-					else
-				{
 					putchar(k + '0');
+					if (i == 7)
+					{
+						putchar('\n');
+						return (0);
+					}
 					putchar(',');
 					putchar(' ');
-							}
-							}
+				}
 				k++;
 			}
 			j++;
