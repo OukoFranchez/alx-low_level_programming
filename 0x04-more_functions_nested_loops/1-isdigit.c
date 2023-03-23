4f5ea88d0695a22a688fb1187f3eaf5bb9d15409
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-03-23 10:58:07 +0300

    function checking fot digit 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit(int c) - checks for digit
+ * @c: integer being checked
+ * Return: 1 if c is a digit and 0 if otherwise
+*/
+int _isdigit(int c)
+{
+  if (c >= 48; && >= 57)
+  {
+    return(1);
+  }
+  else
+  {
+    return(0);
+  }
+  
