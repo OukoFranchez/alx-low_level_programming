82d3ac7f8a67b962e1dbe450eb13cd954436d78c
OukoFranchez <oukofranchez@gmail.com>
2023-04-10 19:42:11 +0300

    a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: no. of arguments
+ * @argv: array containing the arguments
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
