a639975a98b67628fb6ecf6a742d8775a56079fc
OukoFranchez <oukofranchez@gmail.com>
2023-04-11 03:43:58 +0300

    for loop instead of while loop to reduce line numbers

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,12 +5,12 @@
  * main - entry point
  * @argc: no. of arguments
  * @argv: array containing the arguments
- * return: always 0 (success)
+ * Return: always 0 (success)
  */
 int main(int argc, char *argv[])
 {
 	int cents;
-	int num_coins;
+	int num_coins = 0; /* initialize num_coins to 0 */
 
 	if (argc != 2)
 	{
@@ -23,32 +23,23 @@ int main(int argc, char *argv[])
 	if (cents < 0)
 	{
 		printf("0\n");
-
 		return (0);
 	}
-
-	num_coins = 0;
-
-	while (cents > 0)
+	for (; cents > 0; num_coins++)
 	{
 		if (cents >= 25)
 			cents -= 25;
-			num_coins += 1;
 		else if (cents >= 10)
 			cents -= 10;
-			num_coins += 1;
 		else if (cents >= 5 && cents < 10)
 			cents -= 5;
-			cents += 1;
 		else if (cents >= 2)
 			cents -= 2;
-			num_coins += 1;
 		else
 			cents -= 1;
-			num_coins += 1;
 	}
 
-	printf("%d\n",num_coins);
+	printf("%d\n", num_coins);
 	return (0);
 }
 
