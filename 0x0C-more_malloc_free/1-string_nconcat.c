02ea321187ceb5147d337167af0f74ee7eca85f2
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-04-14 00:18:36 +0300

    correction to concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,29 +10,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr_concat;
-	unsigned int len_1 = 0, len_2 = 0, i = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
 	{
-		s1 = "";
+		s[i] = s1[i];
+		i++;
 	}
 
-	while (s1[len_1] != '\0')
-		len_1++;
-	if (s2 == NULL)
-		s2 = "";
-	while (s2[len_2] != '\0')
-		len_2++;
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	ptr_concat = malloc(sizeof(char) * (len_1 + n + 1));
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	for (i = 0; i < len_1; i++)
-		ptr_concat[i] = s1[i];
-	for (i = 0; i < n && s2[i] != '\0'; i++)
-		ptr_concat[len_1 + i] = s2[i];
+	s[i] = '\0';
 
-	ptr_concat[len_1 + i] = '\0';
-
-	return (ptr_concat);
+	return (s);
 }
+
