79a3c1babb40dba9995e2d477bcceb7f23b4e47a
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-05-23 01:43:32 +0300

    updated function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,35 +1,56 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * _strdup - returns a pointer to a newly alloacted space with a copy of arg
- * @str: the string to copy
- *
- * Return: pointer to array or null
+ * _strdup -  returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the string being duplicated
+ * Return: On success, the _strdup function returns a pointer to
+ * the duplicated string.
+ * It returns NULL if insufficient memory was available
  */
-
 char *_strdup(char *str)
 {
-	char *strNew;
-	int x;
-	int y;
+	char *ptr;
+	int len;
+	int i;
 
 	if (str == NULL)
-		return (NULL);
-	x = 0;
-	while (str[x] != '\0')
 	{
-		x++;
-	}
-	x++;
-	strNew = malloc(sizeof(*str) * x);
-	if (strNew == NULL)
 		return (NULL);
-	y = 0;
-	while (str[y] != '\0')
-	{
-		strNew[y] = str[y];
-		y++;
 	}
-	return (strNew);
+
+	len = _strlen(str);
+	ptr = malloc((len + 1) * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[len] = '\0';
+
+	return (ptr);
+	free(ptr);
+
 }
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to get the length of
+ *
+ * Return: The length of @s.
+ */
+
+int _strlen(char *s)
+{
+	int lenStr = 0;
+
+	while (*s++)
+		lenStr++;
+
+	return (lenStr);
+}
+
