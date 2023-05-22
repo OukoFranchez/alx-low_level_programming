debc4cb82bc422ae80e842d56141fd0e5f67a629
OukoFranchez <96543749+OukoFranchez@users.noreply.github.com>
2023-05-23 01:31:11 +0300

    updated function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,106 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
- * str_concat - a function that concatenates two strings
- * @s1: the first string input
- * @s2: the second string input
- *
- * Return: a pointer that contains two strings
+ * str_concat - concatenates two strings
+ * @s1: 1st string
+ * @s2: 2nd string
+ * Return: pointer to the newly allocated space in memory which
+ * contains contents of s1 followed by s2 and null terminated
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *s;
-	int size1 = 0, size2 = 0, i, j;
+	char *ptr;
+	size_t len;
 
-	if (!s1)
+	if (s1 == NULL)
+	{
 		s1 = "";
-	if (!s2)
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
-
-	while (s1[size1] != '\0')
-	{
-		size1++;
 	}
 
-	while (s2[size2] != '\0')
-	{
-		size2++;
-	}
+	len = (_strlen(s1) + _strlen(s2));
 
-	s = (char *) malloc(sizeof(char) * (size1 + size2 + 1));
-	if (s == 0)
-		return (0);
+	ptr = malloc((len + 1) * sizeof(char));
 
-	for (i = 0; i < size1; i++)
+	if (ptr == NULL)
 	{
-		s[i] = s1[i];
+		return (NULL);
 	}
+	_strcpy(ptr, s1);
+	_strcat(ptr, s2);
 
-	for (j = 0; j < size2; j++)
-	{
-		s[i] = s2[j];
-		i++;
-	}
-	return (s);
+
+	/* free(ptr); */
+	return (ptr);
+
 }
+
+/**
+ * *_strcpy - copies string pointed by src to dest
+ * @dest: destination buffer
+ * @src: source string
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: The string to get the length of
+ *
+ * Return: The length of @s.
+ */
+
+int _strlen(char *s)
+{
+	int lenStr = 0;
+
+	while (*s++)
+		lenStr++;
+
+	return (lenStr);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: the pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, count2 = 0;
+
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
+
