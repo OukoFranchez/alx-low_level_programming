e42f4bd2eadbcde09974a4b292d3d6fe0395a908
OukoFranchez <oukofranchez@gmail.com>
2023-10-26 04:19:46 +0300

    refactor: take an alternative approach

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,32 +3,17 @@
 
 
 def island_perimeter(grid):
-    """
-    Calculate the perimeter of the island described in the grid.
-
-    Args:
-    grid (list of list of int): A grid representing land (1) and water (0).
-
-    Returns:
-    int: The perimeter of the island.
-    """
-
-    # Initialize variables to keep track of perimeter and grid dimensions.
+    """ returns the perimeter of an island """
     perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
-
-    # Define the directions (up, down, left, right).
-    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                # For each land cell, check its neighbors.
-                for dr, dc in directions:
-                    nr, nc = row + dr, col + dc
-                    # If the neighbor is out of bounds or a water cell, increment the perimeter.
-                    if nr < 0 or nr >= rows or nc < 0 or nc >= cols or grid[nr][nc] == 0:
-                        perimeter += 1
-
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] == 1:
+                if j + 1 == len(grid[i]) or grid[i][j + 1] == 0:
+                    perimeter += 1
+                if j - 1 == -1 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if i - 1 == -1 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i + 1 == len(grid) or grid[i + 1][j] == 0:
+                    perimeter += 1
     return perimeter
